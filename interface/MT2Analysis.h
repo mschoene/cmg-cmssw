fda5a3c70764c7513237aadbbd2a9724cc316400
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2014-11-07 18:32:53 +0100

    implemented operator=

--- interface/MT2Analysis.h
+++ interface/MT2Analysis.h
@@ -248,6 +248,25 @@ const MT2Analysis<T>& MT2Analysis<T>::operator=( const MT2Analysis& rhs ) {
   htRegions_ = rhs.getHTRegions();
   signalRegions_ = rhs.getSignalRegions();
 
+  for( std::set<MT2HTRegion>::iterator iHT=htRegions_.begin(); iHT!=htRegions_.end(); ++iHT ) {
+    for( std::set<MT2SignalRegion>::iterator iSR=signalRegions_.begin(); iSR!=signalRegions_.end(); ++iSR ) {
+
+      MT2Region thisRegion(*iHT, *iSR);
+
+      T* t1 = this->get(thisRegion); 
+      T* t2 = rhs.get(thisRegion); 
+      if( t2==0 ) {
+        std::cout << "[MT2Analysis::operator+] ERROR! Can't equate MT2Analysis with different regional structures!" << std::endl;
+        exit(111);
+      }
+
+      *t1 = *t2;
+
+    }
+
+  }
+
+
   return *this;
 
 }
