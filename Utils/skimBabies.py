5dd2b540fe8c0c48efb3606b4875e62eab6b5c14
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2014-11-27 12:01:48 +0100

    added command-line configurable cuts and outputdir

--- Utils/skimBabies.py
+++ Utils/skimBabies.py
@@ -5,7 +5,7 @@ from ROOT import TFile, TTree
 
 
 
-def skimBaby( fname, dir, skimdir ):
+def skimBaby( fname, dir, skimdir, cuts ):
 
   fullname = dir+"/"+fname
   file = TFile(fullname)
@@ -14,7 +14,7 @@ def skimBaby( fname, dir, skimdir ):
   print "-> Skimming " + fullname
   newfilename = fname.split(".root")[0] + "_skim.root"
   newfile = TFile(skimdir+"/"+newfilename, "recreate")
-  newtree = tree.CopyTree("mt2>200. && ht>450.")
+  newtree = tree.CopyTree(cuts)
   newfile.cd()
   newtree.Write()
   newfile.Close()
@@ -35,14 +35,24 @@ if __name__ == '__main__':
                       help="skim only selected dataset")
 
    (options,args) = parser.parse_args()
+   if len(args)==0:
+     print "ERROR! Script needs at least one argument to run:"
+     print "   python skimBabies.py [input_directory] [output_subdirectory=\"mt2_200_ht_450\"] [selection=\"mt2>200. && ht>450.\"]"
+     exit()
 
    dir = args[0]
+   subdir = "mt2_200_ht_450"
+   if len(args)>1 : subdir = args[1]
+   cuts = "mt2>200. && ht>450."
+   if len(args)>2 : cuts = args[2]
+
+   print "-> Will apply following skim: " + cuts
 
    if options.filter !="" :
      print "-> Skimming only files containing: " + str(options.filter)
 
 
-   skimdir = dir + "/skim_mt2_200"
+   skimdir = dir + "/" + subdir
    os.system("mkdir -p " + skimdir)
 
    files = os.listdir(dir)
@@ -50,7 +60,7 @@ if __name__ == '__main__':
    for f in files:
      if ".root" in f:
        if options.filter in f:
-         skimBaby(f, dir, skimdir)
+         skimBaby(f, dir, skimdir, cuts)
 
    print "Find skimmed babies in " + skimdir
 
