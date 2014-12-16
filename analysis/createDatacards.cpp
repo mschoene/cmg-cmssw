908af7210e25736168dc26b695a27b7225d68b53
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2014-12-16 16:10:45 +0100

    now should be correctly assigning uncorrelated systs to zinv

--- analysis/createDatacards.cpp
+++ analysis/createDatacards.cpp
@@ -37,7 +37,7 @@ int main( int argc, char* argv[] ) {
   float err_llep_corr   = 0.;
   float err_llep_uncorr = 0.075;
   float err_zinv_corr   = 0.;
-  float err_zinv_uncorr = 0.;
+  float err_zinv_uncorr = 1.0;
   float err_sig_corr    = 0.1;
   float err_sig_uncorr  = 0.;
 
@@ -164,23 +164,19 @@ int main( int argc, char* argv[] ) {
        datacard << " \t " << this_qcd->Integral() << " \t " << this_zinv->Integral() << " \t " << this_llep->Integral() << std::endl;
        datacard << "-------------" << std::endl;
 
-       datacard << "syst_sig    lnN \t 1.1 - - -" << std::endl;
+       datacard << "syst_sig    lnN    1.1 - - -" << std::endl;
 
        int N_llep = (int)this_llep->Integral();
        float llep_stat_err = (N_llep>0) ? 1./sqrt((float)N_llep) : 1.;
        float llep_tot_err = sqrt( llep_stat_err*llep_stat_err + 0.15*0.15 );
        llep_tot_err+=1.;
-       datacard << "syst_llep_corr_" << thisRegion.getName() << "  lnN \t - - - " << llep_tot_err << std::endl;
+       datacard << "syst_llep_corr_" << thisRegion.getName() << "  lnN   - - - " << llep_tot_err << std::endl;
 
 
-       if( thisRegion.nBJetsMin()>=2 )
-         datacard << "syst_zinv_corr_" << thisRegion.getName() << " lnN \t - - 2.0 -" << std::endl;
-       else {
-         datacard << "syst_zinv_corr lnN \t - - 1.2 -" << std::endl;
-         if( thisRegion.nBJetsMin()>0 ) {
-           datacard << "syst_zinv_Z1b_" << thisRegion.getName() << " lnN \t - - 1.3 -" << std::endl;
-         }
-       }
+
+
+
+       // QCD SYSTEMATICS:
 
        if( this_qcd->Integral()>0. ) {
          for( unsigned i=1; i<this_qcd->GetNbinsX()+1; ++i ) 
@@ -188,10 +184,37 @@ int main( int argc, char* argv[] ) {
        }
 
 
+
+
+       // Z INVISIBLE SYSTEMATICS:
+
        if( this_zinv->Integral()>0. ) {
-         for( unsigned i=1; i<this_zinv->GetNbinsX()+1; ++i ) 
-           datacard << this_zinv->GetName() << "_bin_" << i << " shapeN2 - - 1 -" << std::endl;
-       }
+
+         if( thisRegion.nBJetsMin()<2 ) {
+
+           // correlated:
+           datacard << "syst_zinv_corr lnN \t - - 1.2 -" << std::endl;
+           if( thisRegion.nBJetsMin()>0 ) {
+             datacard << "syst_zinv_Z1b_" << thisRegion.getName() << " lnN \t - - 1.3 -" << std::endl;
+           }
+
+           // uncorrelated:
+           for( unsigned i=1; i<this_zinv->GetNbinsX()+1; ++i ) 
+             datacard << this_zinv->GetName() << "_bin_" << i << " shapeN2 - - 1 -" << std::endl; // this is 100% of MC
+
+         } else { // 0 and 1 btags
+
+           for( unsigned i=1; i<this_zinv->GetNbinsX()+1; ++i ) 
+             datacard << this_zinv->GetName() << "_bin_" << i << " shapeN2 - - 1 -" << std::endl; // this is 1/sqrt(k*N)
+
+         }
+
+       } // if zinv
+
+
+
+
+       // LOST LEPTON SYSTEMATICS:
 
        if( this_llep->Integral()>0. ) {
          for( unsigned i=1; i<this_llep->GetNbinsX()+1; ++i ) 
@@ -289,8 +312,9 @@ void writeToTemplateFile_poisson( TFile* file, MT2Analysis<MT2Estimate>* analysi
   
       MT2Region thisRegion(*iHT, *iSR);
 
-      TH1D* h1 = analysis->get( thisRegion )->yield;
-      h1->SetName(Form("%s_%s", h1->GetName(), name.c_str()));
+      TH1D* h1 = analysis->get( thisRegion )->yield->Clone();
+      std::string oldName(h1->GetName());
+      h1->SetName(Form("%s_%s", oldName.c_str(), name.c_str()));
 
       h1->Write();
 
@@ -318,6 +342,9 @@ void writeToTemplateFile_poisson( TFile* file, MT2Analysis<MT2Estimate>* analysi
         h1_binDown->Write();
 
       } // for bins
+
+
+      h1->SetName(oldName.c_str());
 
     } // for SR
   } // for HT
