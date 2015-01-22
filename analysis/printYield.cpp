6c9ac71dc331eaba71f3c57ce61e63b14a5876c2
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2015-01-22 14:53:02 +0100

    using MT2Estimate instead of MT2EstimateSyst, updated to private MT2Analysis name, printing also zinv and llep

--- analysis/printYield.cpp
+++ analysis/printYield.cpp
@@ -1,5 +1,5 @@
 #include "interface/MT2Analysis.h"
-#include "interface/MT2EstimateSyst.h"
+#include "interface/MT2Estimate.h"
 
 #include <iostream>
 #include <sstream>
@@ -27,19 +27,21 @@ int main( int argc, char* argv[] ) {
   std::string firstInputFile  = dir + "/analyses.root";
 
 
-  std::vector< MT2Analysis<MT2EstimateSyst>* > analyses_bg;
-  analyses_bg.push_back( MT2Analysis<MT2EstimateSyst>::readFromFile(firstInputFile.c_str(), "Top") );
-  analyses_bg.push_back( MT2Analysis<MT2EstimateSyst>::readFromFile(firstInputFile.c_str(), "WJets") );
-  analyses_bg.push_back( MT2Analysis<MT2EstimateSyst>::readFromFile(firstInputFile.c_str(), "ZJets") );
-  analyses_bg.push_back( MT2Analysis<MT2EstimateSyst>::readFromFile(firstInputFile.c_str(), "QCD") );
+
+  std::vector< MT2Analysis<MT2Estimate>* > analyses_bg;
+  analyses_bg.push_back( MT2Analysis<MT2Estimate>::readFromFile(firstInputFile.c_str(), "Top") );
+  analyses_bg.push_back( MT2Analysis<MT2Estimate>::readFromFile(firstInputFile.c_str(), "WJets") );
+  analyses_bg.push_back( MT2Analysis<MT2Estimate>::readFromFile(firstInputFile.c_str(), "ZJets") );
+  analyses_bg.push_back( MT2Analysis<MT2Estimate>::readFromFile(firstInputFile.c_str(), "QCD") );
+
 
   for(unsigned a =0;  a < analyses_bg.size(); ++a){
-    std::string ofs = dir + "/yieldtable_" + analyses_bg[a]->name + ".log";
+    std::string ofs = dir + "/yieldtable_" + analyses_bg[a]->getName() + ".log";
     analyses_bg[a]->print(ofs);
   }
 
 
-  MT2Analysis<MT2EstimateSyst>* analysisSum = new MT2Analysis<MT2EstimateSyst>( *(analyses_bg[0]) );
+  MT2Analysis<MT2Estimate>* analysisSum = new MT2Analysis<MT2Estimate>( *(analyses_bg[0]) );
   for(unsigned a =1;  a < analyses_bg.size(); ++a)
     (*analysisSum) += (*(analyses_bg[a]));
 
@@ -47,14 +49,26 @@ int main( int argc, char* argv[] ) {
 
   analysisSum->print(dir + "/yieldtable_FullBkg.log");
 
+
+  std::vector< MT2Analysis<MT2Estimate>* > estimates;
+  estimates.push_back( MT2Analysis<MT2Estimate>::readFromFile(firstInputFile.c_str(), "zinv") );
+  estimates.push_back( MT2Analysis<MT2Estimate>::readFromFile(firstInputFile.c_str(), "llep") );
+  for(unsigned a =0;  a < estimates.size(); ++a){
+    if( estimates[a]==0 ) {
+      std::cout << "-> Looks like you didn't create datacards yet for dir '" << dir << "'. Will not print the estimates." << std::endl;
+      break;
+    }
+    std::string ofs = dir + "/yieldtable_" + estimates[a]->getName() + ".log";
+    estimates[a]->print(ofs);
+  }
   
-  //MT2Analysis<MT2EstimateSyst>* analysisTop = MT2Analysis<MT2EstimateSyst>::readFromFile(firstInputFile.c_str(), "LostLepton");
+  //MT2Analysis<MT2Estimate>* analysisTop = MT2Analysis<MT2Estimate>::readFromFile(firstInputFile.c_str(), "LostLepton");
 
 
-  std::vector < MT2Analysis<MT2EstimateSyst>* > analysesSignal = MT2Analysis<MT2EstimateSyst>::readAllFromFile(firstInputFile.c_str(), "SMS");
+  std::vector < MT2Analysis<MT2Estimate>* > analysesSignal = MT2Analysis<MT2Estimate>::readAllFromFile(firstInputFile.c_str(), "SMS");
   
   for(unsigned a =0;  a < analysesSignal.size(); ++a) {
-    std::string ofs = dir + "/yieldtable_" + analysesSignal[a]->name + ".log";
+    std::string ofs = dir + "/yieldtable_" + analysesSignal[a]->getName() + ".log";
     analysesSignal[a]->print(ofs);
   }
 
