31dab1e599f52aabbf55e7092767f7f815f1bdf6
mmasciov <mario.masciovecchio@cern.ch>
2015-02-13 11:54:33 +0100

    Added extra ht bin

--- interface/MT2Analysis.h
+++ interface/MT2Analysis.h
@@ -215,6 +215,38 @@ MT2Analysis<T>::MT2Analysis( const std::string& aname, const std::string& region
     
     regions_ = multiplyHTandSignal( htRegions, signalRegions );
     
+  } else if( regionsSet=="13TeV_PHYS14_hiHT" ){
+
+    std::set<MT2HTRegion> htRegions;
+    htRegions.insert(MT2HTRegion( 450.,   575.));
+    htRegions.insert(MT2HTRegion( 575.,  1000.));
+    htRegions.insert(MT2HTRegion(1000.,  1500.));
+    htRegions.insert(MT2HTRegion(1500.,    -1 ));
+    
+    std::set<MT2SignalRegion> signalRegions;
+    signalRegions.insert(MT2SignalRegion(2,  3, 0,  0));
+    signalRegions.insert(MT2SignalRegion(4, 6, 0,  0));
+    signalRegions.insert(MT2SignalRegion(7, 8, 0,  0));
+    signalRegions.insert(MT2SignalRegion(9, -1, 0,  0));
+    signalRegions.insert(MT2SignalRegion(2,  3, 1,  1));
+    signalRegions.insert(MT2SignalRegion(4, 6, 1,  1));
+    signalRegions.insert(MT2SignalRegion(7, 8, 1,  1));
+    signalRegions.insert(MT2SignalRegion(9, -1, 1,  1));
+    signalRegions.insert(MT2SignalRegion(2,  3, 2,  2, "loMT"));
+    signalRegions.insert(MT2SignalRegion(2,  3, 2,  2, "hiMT"));
+    signalRegions.insert(MT2SignalRegion(4, 6, 2,  2, "loMT"));
+    signalRegions.insert(MT2SignalRegion(4, 6, 2,  2, "hiMT"));
+    signalRegions.insert(MT2SignalRegion(7, 8, 2,  2, "loMT"));
+    signalRegions.insert(MT2SignalRegion(7, 8, 2,  2, "hiMT"));
+    signalRegions.insert(MT2SignalRegion(9, -1, 2,  2, "loMT"));
+    signalRegions.insert(MT2SignalRegion(9, -1, 2,  2, "hiMT"));
+    signalRegions.insert(MT2SignalRegion(2,  6, 3,  -1, "loMT"));
+    signalRegions.insert(MT2SignalRegion(2,  6, 3,  -1, "hiMT"));
+    signalRegions.insert(MT2SignalRegion(7, -1, 3,  -1, "loMT"));
+    signalRegions.insert(MT2SignalRegion(7, -1, 3,  -1, "hiMT"));
+
+    regions_ = multiplyHTandSignal( htRegions, signalRegions );
+
   } else if( regionsSet=="13TeV_PHYS14_noMT" ){
 
     std::set<MT2HTRegion> htRegions;
